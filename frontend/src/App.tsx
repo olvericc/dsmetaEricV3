c82abd8c23a0e98ac257417e145a981946f32289
olvericc <olv.ericlopes@gmail.com>
2022-11-14 13:07:28 -0300

    creating the first component in react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
-  return(<h1>Hello World!</h1>);
+  return (
+    <>
+      <h1>Title</h1>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+    </>
+  );
 }
 
 export default App
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import icon from "../../assets/img/notification-icon.svg"
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <>
+            <div className="dsmeta-red-btn">
+                <img src={icon} alt="Notify" />
+            </div>
+        </>
+    );
+}
+
+export default NotificationButton
