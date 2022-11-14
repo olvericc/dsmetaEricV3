dfe87ac4f00dc2307a0bb81aa1bfa28ad541a766
olvericc <olv.ericlopes@gmail.com>
2022-11-14 12:22:36 -0300

    project created config frontend-backend

---
+++ backend/src/main/java/com/devsuperior/dsmeta2/Dsmeta2Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsmeta2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsmeta2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmeta2/Dsmeta2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsmeta2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
