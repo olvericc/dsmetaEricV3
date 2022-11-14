9ba1de07844f58e05819e7d00dfd132880601eed
olvericc <olv.ericlopes@gmail.com>
2022-11-14 13:41:46 -0300

    adding others components in application

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from "../../assets/img/logo.svg"
+import './styles.css'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={logo} alt="DS Meta" />
+                    <h1>DS Meta</h1>
+                    <p>
+                        Developed by
+                        <a href="https://github.com/olvericc"> @olvericc</a>
+                    </p>
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default Header
