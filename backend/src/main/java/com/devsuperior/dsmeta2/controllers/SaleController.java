ef1725c209e1c94900b88fab709e25ac6b07f8c8
olvericc <olv.ericlopes@gmail.com>
2022-11-16 12:24:25 -0300

    creating controller and testing in Postman

---
+++ backend/src/main/java/com/devsuperior/dsmeta2/controllers/SaleController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dsmeta2.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmeta2.entities.Sale;
+import com.devsuperior.dsmeta2.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping
+	public List<Sale> findSales() {
+		return service.findSales();
+	}
+}
