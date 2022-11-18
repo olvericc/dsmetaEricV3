912e067393fde22d4295985a0f8e5a7762ab8cdc
olvericc <olv.ericlopes@gmail.com>
2022-11-18 09:20:02 -0300

    using react hook useEffect to make backend requests

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,7 +2,8 @@ import NotificationButton from '../NotificationButton';
 import './styles.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function SalesCard() {
     
@@ -12,6 +13,16 @@ function SalesCard() {
 
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
+
+    // React Hook - useEffect (requisition)
+    useEffect(
+        () => {
+            axios.get("http://localhost:8080/sales")
+                .then(response => {
+                    console.log(response.data)
+            });
+        }, []
+    );
     
     return (
         <>
